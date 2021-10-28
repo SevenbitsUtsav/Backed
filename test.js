ff685be107a7f20a1582cd07c4bb36bd1b0098b9
Utsav <utsav@7bits.in>
2021-10-28 13:22:15 +0530

    Checkaddress Solved

--- test.js
+++ test.js
@@ -32,95 +32,101 @@ function getLastSyncedBlock() {
     });
 };
 //getLastSyncedBlock()
+
+
 function checkaddress(adr) {
     return new Promise(function(resolve, reject) {
         console.log(adr, 'test')
         db.query(`SELECT address FROM clientsaddress WHERE address = '${adr}'`,
-            // function(error, result) {
-            //     console.log(result)
-            //         //console.debug(error, result);
-            //     if (result.length == 0) {
-            //         //console.debug(`[DB] there is no any address.`);
-            //         return resolve(false);
-            //     } else {
-            //         //console.log(result)
-            //         //console.debug('address:', result[0]);
-            //         return resolve(true);
-            //     }
-            // });
             function(error, result) {
-                console.debug(error, result);
-                if (error || !result) {
-                    console.error('[DB] errored in getting last block.');
-                    return reject(error);
-                } else if (result.length == 0) {
-                    console.debug(`[DB] there is no any block.`);
+                console.log(result)
+                    //console.debug(error, result);
+                if (result.rowCount == 0) {
+                    //console.debug(`[DB] there is no any address.`);
                     return resolve(false);
                 } else {
                     //console.log(result)
-                    console.debug('[DB] got the last processed block', result.rows);
+                    console.debug('address:', result);
                     return resolve(true);
                 }
             });
+        // function(error, result) {
+        //     console.debug(error, result);
+        //     if (error || !result) {
+        //         console.error('[DB] errored in getting last block.');
+        //         return reject(error);
+        //     } else if (result.length == 0) {
+        //         console.debug(`[DB] there is no any block.`);
+        //         return resolve(false);
+        //     } else {
+        //         //console.log(result)
+        //         console.debug('[DB] got the last processed block', result.rows);
+        //         return resolve(true);
+        //     }
+        // });
     });
 }
 async function insertBlocksandtrans() {
-    var x = 27794900 //await getLastSyncedBlock();
-    const b = 27794903 //await web3.eth.getBlockNumber()
-    console.log('letest block number ', b)
-    for (let i = x; i < b; i++) {
-        let block = await web3.eth.getBlock(i);
-        let number = block.number
-            //console.log(block)
-        console.log('serching block ' + number);
-        console.log('current block have a ', block.transactions.length, 'transactions')
-        if (block && block.transactions) {
-            for (let txHash of block.transactions) {
-                let tx = await web3.eth.getTransaction(txHash);
-                //console.log(tx)
+    try {
+        var x = 27794900 //await getLastSyncedBlock();
+        const b = 27794903 //await web3.eth.getBlockNumber()
+        console.log('letest block number ', b)
+        for (let i = x; i < b; i++) {
+            let block = await web3.eth.getBlock(i);
+            let number = block.number
+                //console.log(block)
+            console.log('serching block ' + number);
+            console.log('current block have a ', block.transactions.length, 'transactions')
+            if (block && block.transactions) {
+                for (let txHash of block.transactions) {
+                    let tx = await web3.eth.getTransaction(txHash);
+                    //console.log(tx)
 
-                var account = await checkaddress(tx.to)
-                if (account) {
-                    console.log(`Transaction found on block ${ number }`);
-                    console.log(`Address is ${ tx.to }`);
-                    console.log('value is ', web3.utils.fromWei(tx.value, 'ether'), ' Ether'),
-                        console.log(`Timestamp is ${ new Date() }`)
-                    console.log(tx.hash)
-                    let recipent = await web3.eth.getTransactionReceipt(tx.hash)
-                    console.log(recipent.status)
-                    return new Promise(function(resolve, reject) {
-                        const sqlQuery2 = `INSERT INTO transactions (hash, blockNumber, BlockHash, "from", "to", amount, gasPrice, transactionIndex,gas, nonce, status) VALUES`;
-                        const sqlQuery3 = sqlQuery2 + `('${tx.hash}', ${tx.blockNumber}, '${tx.blockHash}', '${tx.from}', '${tx.to}', '${web3.utils.fromWei(tx.value)}', '${tx.gasPrice}', '${tx.transactionIndex}','${tx.gas}', '${tx.nonce}', '${recipent.status}')`
-                        console.debug("sqlQuery3", sqlQuery3);
-                        db.query(sqlQuery3,
-                            function(error, result) {
-                                //console.debug(error, result);
-                                if (error || !result) {
-                                    //  console.error();
-                                    //  return reject(error);
-                                } else {
-                                    console.debug('[DB] Transaction inserted into table, affectedRows:', result.affectedRows);
-                                    return (result.affectedRows);
-                                }
-                            })
-                    })
+                    var account = await checkaddress(tx.to)
+                    if (account) {
+                        console.log(`Transaction found on block ${ number }`);
+                        console.log(`Address is ${ tx.to }`);
+                        console.log('value is ', web3.utils.fromWei(tx.value, 'ether'), ' Ether'),
+                            console.log(`Timestamp is ${ new Date() }`)
+                        console.log(tx.hash)
+                        let recipent = await web3.eth.getTransactionReceipt(tx.hash)
+                        console.log(recipent.status)
+                        return new Promise(function(resolve, reject) {
+                            const sqlQuery2 = `INSERT INTO transaction (hash, blockNumber, BlockHash, "from", "to", amount, gasPrice, transactionIndex,gas, nonce, status) VALUES`;
+                            const sqlQuery3 = sqlQuery2 + `('${tx.hash}', ${tx.blockNumber}, '${tx.blockHash}', '${tx.from}', '${tx.to}', '${web3.utils.fromWei(tx.value)}', '${tx.gasPrice}', '${tx.transactionIndex}','${tx.gas}', '${tx.nonce}', '${recipent.status}')`
+                            console.debug("sqlQuery3", sqlQuery3);
+                            db.query(sqlQuery3,
+                                function(error, result) {
+                                    //console.debug(error, result);
+                                    if (error || !result) {
+                                        console.error();
+                                        return reject(error);
+                                    } else {
+                                        console.debug('[DB] Transaction inserted into table, affectedRows:', result.rowCount);
+                                        return (result.rowCount);
+                                    }
+                                })
+                        })
+                    }
                 }
             }
+            /*           const sqlQuery1 = `INSERT INTO blocks (number, hash, parentHash, blockTimestamp) VALUES ('${block.number}', '${block.hash}', '${block.parentHash}','${block.timestamp}')`
+                          console.debug("sqlQuery1", sqlQuery1);
+                          db.query(sqlQuery1,
+                              function (error, result) {
+                          console.debug(error, result);
+                          if (error || !result) {
+                              console.error();
+                              return reject(error);
+                          } else {
+                              console.debug('[DB] Block inserted into table affectedRows:', result.affectedRows);
+                              return (result.affectedRows);
+                          }››
+                          }
+                          ) */
         }
-        /*           const sqlQuery1 = `INSERT INTO blocks (number, hash, parentHash, blockTimestamp) VALUES ('${block.number}', '${block.hash}', '${block.parentHash}','${block.timestamp}')`
-                      console.debug("sqlQuery1", sqlQuery1);
-                      db.query(sqlQuery1,
-                          function (error, result) {
-                      console.debug(error, result);
-                      if (error || !result) {
-                          console.error();
-                          return reject(error);
-                      } else {
-                          console.debug('[DB] Block inserted into table affectedRows:', result.affectedRows);
-                          return (result.affectedRows);
-                      }››
-                      }
-                      ) */
+    } catch (err) {
+        console.log(err)
     }
 };
 insertBlocksandtrans()
